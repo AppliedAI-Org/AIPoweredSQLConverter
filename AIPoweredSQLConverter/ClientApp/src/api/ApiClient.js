a69437be2b82b48aed36362f167c1272a04d73c8
Jacob-J-Thomas <92639671+Jacob-J-Thomas@users.noreply.github.com>
2025-04-18 18:44:24 -0500

    Removed auth0 completely from all files to ensure its not possible to call the auth endpoint

--- AIPoweredSQLConverter/ClientApp/src/api/ApiClient.js
+++ AIPoweredSQLConverter/ClientApp/src/api/ApiClient.js
@@ -9,9 +9,8 @@ const DefaultErrorMessage = "Something went wrong. If you continue to receive th
 const NullTableErrorMessage = "Please ensure you've attached SQL table schema(s) to the left.";
 const TooManyRequestsMessage = "Sorry, but it looks like your usage quota has been depleted. To sign up for pay as you go billing, you can do so here: "
 class ApiClient {
-    constructor(baseUrl, getAccessTokenSilently) {
+    constructor(baseUrl) {
         this.baseUrl = baseUrl;
-        this.getAccessTokenSilently = getAccessTokenSilently;
         this.client = axios.create({
             baseURL: baseUrl,
         });
